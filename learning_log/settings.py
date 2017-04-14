89a03b37a0ee30d356c0a430b917b2343b361c57
Jasiel Serra <jasiel_serra@hotmail.com>
2017-04-14 16:44:47 -0300

    Install learning_logs app and apply migrations

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'learning_log.learning_logs',
 )
 
 MIDDLEWARE_CLASSES = (
