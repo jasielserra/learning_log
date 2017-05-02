aa421f09682d0c5ce93990ba46ae2ac4fcc8ad28
Jasiel Serra <jasiel_serra@hotmail.com>
2017-05-01 21:08:53 -0300

    Recfatoring check_topic_owner

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -23,12 +23,19 @@ def topic(request, topic_id):
     """ Show a single topic, and all its entries."""
     topic = Topic.objects.get(id=topic_id)
     # Make sure the topic belongs to the current user.
-    if topic.owner != request.user:
+    if not check_topic_owner(topic, request.user):
         raise Http404
 
+    #if topic.owner != request.user:
+    #    raise Http404
+
     entries = topic.entry_set.order_by('-date_added')
-    context = {'topic': topic, 'entries':entries}
-    return render(request,'learning_logs/topic.html', context)
+    context = {'topic': topic, 'entries': entries}
+
+    return render(request, 'learning_logs/topic.html', context)
+
+
+
 
 @login_required
 def new_topic(request):
@@ -72,8 +79,11 @@ def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
     # Make sure the topic belongs to the current user.
-    if topic.owner != request.user:
+    if not check_topic_owner(topic, request.user):
         raise Http404
+
+    #if topic.owner != request.user:
+    #    raise Http404
 
     if request.method != 'POST':
         # Initial request, pre-fill form with the current entry.
@@ -88,5 +98,10 @@ def edit_entry(request, entry_id):
     context = {'entry':entry, 'topic':topic,'form':form}
 
     return render(request, 'learning_logs/edit_entry.html', context)
+
+
+def check_topic_owner(topic_owner, user):
+    return topic_owner.owner == user
+
 
 
